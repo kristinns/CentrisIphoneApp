09fc1ec0d8815d9d6577aba9b730d6f2a3898d01
Kristinn Svansson <krsvans@gmail.com>
2013-08-18 17:47:43 +0000

    Add Assignment model

---
+++ Centris/Models/Assignment.h
@@ -0,0 +1,18 @@
+//
+//  Assignment.h
+//  Centris
+//
+//  Created by Kristinn Svansson on 8/18/13.
+//  Copyright (c) 2013 Kristinn Svansson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Assignment : NSObject
+
+@property (strong, nonatomic) NSString *title;
+@property (strong, nonatomic) NSDate *deadline;
+@property (strong, nonatomic) NSDate *submitted; // If not submitted, this should point to NULL
+@property (strong, nonatomic) NSString *description;
+
+@end
