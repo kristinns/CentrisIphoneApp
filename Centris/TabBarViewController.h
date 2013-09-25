8436e6b9a649947b72d74136233b4758dc393dd2
Bjarki Sörens <bjerkins8@gmail.com>
2013-09-25 22:03:07 +0000

    Setting default tab to be “Veitan”

---
+++ Centris/TabBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  TabBarViewController.h
+//  Centris
+//
+//  Created by Bjarki Sörens on 9/25/13.
+//  Copyright (c) 2013 Kristinn Svansson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabBarViewController : UITabBarController
+
+@end
