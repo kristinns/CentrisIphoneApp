f618889d1d23766a2804ba3859bd5f5f73f70f49
Bjarki Sörens <bjerkins8@gmail.com>
2013-09-26 12:34:27 +0000

    HTML-parser included

---
+++ Centris/CafeteriaViewController.h
@@ -0,0 +1,13 @@
+//
+//  CafeteriaViewController.h
+//  Centris
+//
+//  Created by Bjarki Sörens on 9/26/13.
+//  Copyright (c) 2013 Kristinn Svansson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CafeteriaViewController : UITableViewController
+
+@end
