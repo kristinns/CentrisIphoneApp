3f0a1aeed496fa4cacc87f2a36cc34dd0d47fd88
Bjarki Sorens <bjerkins8@gmail.com>
2013-11-18 17:45:47 +0000

    added assignment files to core data

---
+++ Centris/Models/AssignmentFiles.h
@@ -0,0 +1,22 @@
+//
+//  AssignmentFiles.h
+//  Centris
+//
+//  Created by Bjarki Sörens on 18/11/13.
+//  Copyright (c) 2013 Kristinn Svansson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@class Assignment;
+
+@interface AssignmentFiles : NSManagedObject
+
+@property (nonatomic, retain) NSString * url;
+@property (nonatomic, retain) NSString * fileName;
+@property (nonatomic, retain) NSDate * lastUpdate;
+@property (nonatomic, retain) NSString * type;
+@property (nonatomic, retain) Assignment *isInAssignment;
+
+@end
---
+++ Centris/Models/AssignmentFiles.m
@@ -0,0 +1,21 @@
+//
+//  AssignmentFiles.m
+//  Centris
+//
+//  Created by Bjarki Sörens on 18/11/13.
+//  Copyright (c) 2013 Kristinn Svansson. All rights reserved.
+//
+
+#import "AssignmentFiles.h"
+#import "Assignment.h"
+
+
+@implementation AssignmentFiles
+
+@dynamic url;
+@dynamic fileName;
+@dynamic lastUpdate;
+@dynamic type;
+@dynamic isInAssignment;
+
+@end
